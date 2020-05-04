13a2339d9a6774e9664abfb177351e5ce54f9444
NatClamp <nat.clamp@gmail.com>
2020-05-04 12:26:19 +0100

    👷🏼‍♀️

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-var buttonsArray = document.getElementsByTagName('button');
-var header = document.getElementById('name');
-var linkArray = document.getElementsByTagName('a');
-var activeButton = document.getElementsByClassName('button--active')
-var workButton = document.querySelector('#button-work');
-var previouslyButton = document.querySelector('#button-previously');
-var workContent = document.querySelector('#work');
-var previouslyContent = document.querySelector('#previously');
+let buttonsArray = document.getElementsByTagName('button');
+let header = document.getElementById('name');
+let linkArray = document.getElementsByTagName('a');
+let activeButton = document.getElementsByClassName('button--active')
+let workButton = document.querySelector('#button-work');
+let previouslyButton = document.querySelector('#button-previously');
+let workContent = document.querySelector('#work');
+let previouslyContent = document.querySelector('#previously');
+let stillImage = document.getElementById('face-still');
+let movingImage = document.getElementById('face-gif');
+
+imageHover()
 
 Array.prototype.forEach.call(buttonsArray, button => {
   button.addEventListener('click', () => {
@@ -26,4 +30,15 @@ function removeButtonsActiveState() {
   Array.prototype.forEach.call(buttonsArray, button => {
     if (button.classList.contains('button--active')) button.classList.remove('button--active')
   });
+}
+
+function imageHover() {
+  stillImage.addEventListener('mouseenter', () => {
+    stillImage.classList.add('hidden');
+    movingImage.classList.remove('hidden');
+  })
+  stillImage.addEventListener('mouseleave', () => {
+    stillImage.classList.remove('hidden');
+    movingImage.classList.add('hidden');
+  })
 }
